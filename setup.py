679eae391fafd62f5f392d5067714361c0f8e31e
readysloth <readysloth@protonmail.com>
2023-07-27 13:07:27 +0300

    add(setup.py)

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from setuptools import setup
+
+PACKAGE_NAME = 'fill'
+
+setup(name=PACKAGE_NAME,
+      version='0.0.1',
+      description='Fills clicked fields with strings from file or stdin',
+      install_requires=['pynput==1.7.6'],
+      entry_points={'console_scripts': ['fill = main:run_default']})
